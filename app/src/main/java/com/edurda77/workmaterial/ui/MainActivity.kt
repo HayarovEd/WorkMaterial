29a7346650b523ccec55d71596daed149f5cfafe
HayarovEd <edurd@rambler.ru>
2021-12-14 12:01:38 +0500

    add fragment_basic.xml

--- app/src/main/java/com/edurda77/workmaterial/ui/MainActivity.kt
+++ app/src/main/java/com/edurda77/workmaterial/ui/MainActivity.kt
@@ -3,10 +3,13 @@ package com.edurda77.workmaterial.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.edurda77.workmaterial.R
+import com.edurda77.workmaterial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
