697de64cd72df8b7df449f28d307fea7d0c39647
HayarovEd <edurd@rambler.ru>
2021-12-14 20:13:33 +0500

    maked load Image of Day

--- app/src/main/java/com/edurda77/workmaterial/model/NasaRepoUseCaseImpl.kt
+++ app/src/main/java/com/edurda77/workmaterial/model/NasaRepoUseCaseImpl.kt
@@ -1,52 +1,33 @@
 package com.edurda77.workmaterial.model
-
-import com.edurda77.workmaterial.BuildConfig.NASA_API_KEY
 import com.edurda77.workmaterial.domain.NasaApi
-import com.edurda77.workmaterial.domain.NasaRepoUseCase
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 const val BASE_URL = "https://api.nasa.gov/"
 
-class NasaRepoUseCaseImpl : NasaRepoUseCase {
-    private var retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-    private var api: NasaApi = retrofit.create(NasaApi::class.java)
-    override fun getFotoOfTheDaySync(): PODServerResponseData? {
+class NasaRepoUseCaseImpl  {
 
-        return api.getPictureOfTheDay(NASA_API_KEY)
-            .execute().body()
+    fun getNasaService(): NasaApi {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(createOkHttpClient())
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
+            .build()
+
+        return retrofit.create(NasaApi::class.java)
     }
 
-    override fun getFotoOfTheDayAsync(
-        onSuccess: (PODServerResponseData?) -> Unit,
-        OnError: (Throwable) -> Unit
-    ) {
-        api.getPictureOfTheDay(NASA_API_KEY).enqueue(object :
-            Callback<PODServerResponseData> {
-            override fun onResponse(
-                call: Call<PODServerResponseData>,
-                response: Response<PODServerResponseData>
-            ) {
-                if (response.isSuccessful) {
-                    onSuccess(response.body()!!)
-                } else {
-                    OnError(Throwable("Неизвестная ошибка"))
-                }
-
-            }
-
-            override fun onFailure(call: Call<PODServerResponseData>, t: Throwable) {
-                OnError(t)
-            }
-
-
-        })
+    private fun createOkHttpClient(): OkHttpClient {
+        val httpClient = OkHttpClient.Builder()
+        val loggingInterceptor = HttpLoggingInterceptor().setLevel(
+            HttpLoggingInterceptor.Level.BODY
+        )
+        httpClient.addInterceptor(loggingInterceptor)
+        return httpClient.build()
     }
+
 
 }
