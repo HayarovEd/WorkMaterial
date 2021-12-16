e1aa647d96e5a03c6e9e4b9843f709b5517bf5fe
HayarovEd <edurd@rambler.ru>
2021-12-16 19:39:35 +0500

    release Navigation Drawer

---
+++ app/src/main/java/com/edurda77/workmaterial/ui/BottomNavigationDrawerFragment.kt
@@ -0,0 +1,37 @@
+package com.edurda77.workmaterial.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.edurda77.workmaterial.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.navigation.NavigationView
+
+class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
+
+    private lateinit var navigationView: NavigationView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dialog_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navigationView = view.findViewById(R.id.navigation_view)
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.navigation_one -> Toast.makeText(context, "1", Toast.LENGTH_SHORT).show()
+                R.id.navigation_two -> Toast.makeText(context, "2", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+    }
+}
