1a12f7b134be833d98b630a971f383f9d6e01eab
HayarovEd <edurd@rambler.ru>
2021-12-16 20:50:41 +0500

    change icons

--- app/src/main/java/com/edurda77/workmaterial/domain/NasaApi.kt
+++ app/src/main/java/com/edurda77/workmaterial/domain/NasaApi.kt
@@ -7,5 +7,15 @@ import retrofit2.http.Query
 
 interface NasaApi {
     @GET("planetary/apod")
-    fun getPictureOfTheDay(@Query("api_key") apiKey: String): Call<PODServerResponseData>
+    fun getPictureOfTheDay(
+        @Query("api_key") apiKey: String,
+        //@Query("date") date: String
+    ): Call<PODServerResponseData>
+
+    fun getPictureOfTheLastDay(
+        @Query("api_key") apiKey: String,
+        @Query("date") date: String,
+    ): Call<PODServerResponseData>
+
+    //fun getPictureOfTheBeforeYesterday(@Query("api_key") apiKey: String): Call<PODServerResponseData>
 }
