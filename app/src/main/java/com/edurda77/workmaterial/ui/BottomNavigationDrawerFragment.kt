4bf6b49af894a5274e105b949c32690ab4e47734
Эдуард <edurd@rambler.ru>
2021-12-18 16:02:41 +0500

    release change themes

--- app/src/main/java/com/edurda77/workmaterial/ui/BottomNavigationDrawerFragment.kt
+++ app/src/main/java/com/edurda77/workmaterial/ui/BottomNavigationDrawerFragment.kt
@@ -30,12 +30,8 @@ class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
         navigationView.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.navigation_one -> {
-                    val createCustomFragment = CustomFragment()
-                    val ft = fragmentManager?.beginTransaction()
-                    ft?.replace(R.id.fragment_container_view, createCustomFragment)
-                    ft?.commit()
-
-                }
+                val intent = Intent(context, CustomActivity::class.java)
+                startActivity(intent)}
                 R.id.navigation_two -> Toast.makeText(context, "2", Toast.LENGTH_SHORT).show()
             }
             true
