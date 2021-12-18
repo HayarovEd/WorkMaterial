ca9f7d0123e55dcf0d0bb62725e3a1535d0fba5d
Эдуард <edurd@rambler.ru>
2021-12-18 16:02:41 +0500

    release change themes

--- app/src/main/java/com/edurda77/workmaterial/ui/MainActivity.kt
+++ app/src/main/java/com/edurda77/workmaterial/ui/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.edurda77.workmaterial.ui
 
+
+
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.add
@@ -10,8 +13,10 @@ import com.edurda77.workmaterial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var currentTheme = R.style.Theme_WorkMaterial
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setTheme(currentTheme)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         if (savedInstanceState==null){
