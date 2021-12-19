32959708d0f4b22120d7e1981ef2d100e4b12594
Эдуард <edurd@rambler.ru>
2021-12-19 12:29:20 +0500

    change themes - not jet released

---
+++ app/src/main/java/com/edurda77/workmaterial/ui/CustomFragment.kt
@@ -0,0 +1,54 @@
+package com.edurda77.workmaterial.ui
+
+import android.content.Intent
+import android.content.res.Resources
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.edurda77.workmaterial.R
+
+
+class CustomFragment : Fragment() {
+   private lateinit var buttonStandart: Button
+    private lateinit var buttonSpace: Button
+    private lateinit var buttonMoon: Button
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_custom, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+        buttonStandart = view.findViewById(R.id.standart_theme)
+        buttonSpace = view.findViewById(R.id.space_theme)
+        buttonMoon = view.findViewById(R.id.moon_theme)
+        setTheme()
+    }
+    private fun setTheme() {
+        buttonStandart.setOnClickListener {
+            R.style.Theme_WorkMaterial
+            requireActivity().recreate()
+        }
+        buttonSpace.setOnClickListener {
+            R.style.Theme_Space
+            requireActivity().recreate()
+           val intent = Intent(context, MainActivity::class.java)
+            intent.putExtra(Resources.Theme::class.java.simpleName, R.style.Theme_Space)
+
+            startActivity(intent)
+
+        }
+        buttonMoon.setOnClickListener {
+            R.style.Theme_Moon
+            requireActivity().recreate()
+        }
+    }
+}
+
--- app/src/main/java/com/edurda77/workmaterial/ui/MainActivity.kt
+++ app/src/main/java/com/edurda77/workmaterial/ui/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.edurda77.workmaterial.ui
 
 
-
 import android.content.res.Resources
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.edurda77.workmaterial.R
@@ -13,18 +14,19 @@ import com.edurda77.workmaterial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var currentTheme = R.style.Theme_WorkMaterial
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setTheme(currentTheme)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        if (savedInstanceState==null){
+        if (savedInstanceState == null) {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 add<BasicFragment>(R.id.fragment_container_view)
 
             }
         }
+
     }
+
 }
