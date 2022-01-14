1ed7278fe87d4193c5e2a5faaf6bde5124e7ada3
HayarovEd <edurd@rambler.ru>
2022-01-14 11:30:22 +0500

    optimaze RoomService

--- app/src/main/java/com/edurda77/workmaterial/model/RoomService.kt
+++ app/src/main/java/com/edurda77/workmaterial/model/RoomService.kt
@@ -2,35 +2,35 @@ package com.edurda77.workmaterial.model
 
 import android.content.Context
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
 import androidx.room.Room
 import com.edurda77.blocknote2021.domain.NoteDao
 
 private const val DB_PATH = "notes.db"
 
-class RoomService(context: Context) : NoteDao {
+class RoomService(context: Context) {
     private val noteDao: NoteDao = Room.databaseBuilder(
         context,
         NoteRoomDb::class.java,
         DB_PATH
     ).build().noteDao()
 
-    override fun add(note: ModelNote) {
+    fun add(note: ModelNote) {
         noteDao.add(note)
     }
 
-    override fun delete(id: Int) {
+    fun delete(id: Int) {
         noteDao.delete(id)
     }
 
-    override fun update(id: Int, title: String, content: String) {
+    fun update(id: Int, title: String, content: String) {
         noteDao.update(id, title, content)
     }
 
-    override fun getNotesLiveData(): LiveData<List<ModelNote>> {
+    fun getNotesLiveData(): LiveData<List<ModelNote>> {
         return noteDao.getNotesLiveData()
     }
 
-    fun getNoteDao() {
-        //TODO Реализовать и удалить наследование
-    }
 }
