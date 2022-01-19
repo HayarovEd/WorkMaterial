e649a92e02739596dad8fb222423779d5df92912
HayarovEd <87774233+HayarovEd@users.noreply.github.com>
2022-01-19 10:58:55 +0500

    Revert "Lesson 2"

--- app/src/main/java/com/edurda77/workmaterial/ui/BottomNavigationDrawerFragment.kt
+++ app/src/main/java/com/edurda77/workmaterial/ui/BottomNavigationDrawerFragment.kt
@@ -1,6 +1,5 @@
 package com.edurda77.workmaterial.ui
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -29,12 +28,7 @@ class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
 
         navigationView.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.navigation_one -> {
-
-
-                    val intent = Intent(context, CustomActivity::class.java)
-                    startActivity(intent)
-                }
+                R.id.navigation_one -> Toast.makeText(context, "1", Toast.LENGTH_SHORT).show()
                 R.id.navigation_two -> Toast.makeText(context, "2", Toast.LENGTH_SHORT).show()
             }
             true
---
+++ app/src/main/java/com/edurda77/workmaterial/ui/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.edurda77.workmaterial.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.edurda77.workmaterial.R
+import com.edurda77.workmaterial.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null){
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<BasicFragment>(R.id.fragment_container_view)
+
+            }
+        }
+    }
+}
